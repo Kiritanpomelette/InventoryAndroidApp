13afaade3e3c59939e184cb0081fe7a4d4e80f8c
taishi <batapi3939@gmail.com>
2023-10-18 12:37:47 +0900

    アダプターセット

--- app/src/main/java/xyz/miyayu/inventorymanager/RecyclerAdapter.kt
+++ app/src/main/java/xyz/miyayu/inventorymanager/RecyclerAdapter.kt
@@ -1,4 +1,32 @@
 package xyz.miyayu.inventorymanager
 
-class RecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter: RecyclerView.Adapter<ViewHolderItem>(){
+    //5)　表示するデータを用意
+    private val nameList = listOf("My Name0","My Name1","My Name2","My Name3","My Name4","My Name5")
+    private val messageList = listOf("My Message0","My Message1","My Message2","My Message3","My Message4","My Message5")
+    private val imageList = listOf(R.drawable.img0,R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4,R.drawable.img5)
+    //4) ここで1行分のレイアウト(view)を作る
+    // (「2」と「3」を紐付ける作業)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderItem {
+        //「2」のレイアウトを取得(インフレート)
+        val itemXml = LayoutInflater.from(parent.context)
+            .inflate(R.layout.one_layout, parent, false)
+        return ViewHolderItem(itemXml)
+    }
+        //5) position番目のデータをレイアウト(xml)に表示させるようにセット
+        override fun onBindViewHolder(holder: ViewHolderItem, position: Int) {
+            holder.tvNameHolder.text = nameList[position]
+            holder.tvMessageHolder.text = messageList[position]
+            holder.ivHolder.setImageResource(imageList[position])
+        }
+        //6) データが何件あるかを確認する
+        override fun getItemCount(): Int {
+            return nameList.size
+        }
+
+
 }
